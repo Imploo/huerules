ec05db0770e717d5d73c9839c9cd978ed9c62f6d
Matt <matthias.bultje@topicus.nl>
2018-08-14 22:52:42 +0200

    Minor fixes/updates

--- src/app/rules/rules-list.component.ts
+++ src/app/rules/rules-list.component.ts
@@ -1,29 +1,28 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { RulesService } from './rules.service';
-import { IRule, ICondition, IAction, IBody } from './rule';
-import { Observable } from 'rxjs/Observable';
-import { Response } from '@angular/http';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {RulesService} from './rules.service';
+import {IAction, IBody, ICondition, IRule} from './rule';
+import {Response} from '@angular/http';
 
 @Component({
     moduleId: module.id,
-    selector: 'rules-list',
+    selector: 'app-rules-list',
     templateUrl: 'rules-list.component.html',
     styleUrls: ['rules-list.component.css'],
     encapsulation: ViewEncapsulation.None
 })
 export class RulesListComponent implements OnInit {
-    
+
     rules: IRule[];
     errorMessage: string;
     operators: string[] = ['eq', 'lt', 'gt', 'dx', 'ddx', 'in'];
 
     getObjectKeys = Object.keys;
 
-    constructor(private _rulesService: RulesService){
+    constructor(private _rulesService: RulesService) {
     }
-    
+
     ngOnInit(): void {
-        this.errorMessage = "";
+        this.errorMessage = '';
         this.initRules();
     }
 
@@ -44,36 +43,39 @@ export class RulesListComponent implements OnInit {
     save(rule: IRule): void {
         this._rulesService.save(rule)
         .subscribe(
-            ok => 
-            {
-                this.parseMessage(ok);                
-                if (!this.errorMessage.includes("error"))
+            ok => {
+                this.parseMessage(ok);
+                if (!this.errorMessage.includes('error')) {
                     this.initRules();
+                }
             }, this.parseMessage);
     }
 
     private parseMessage = (error: Response | any): void => {
-        if (error.type === 3) return;
-        let message = error.message || JSON.stringify(error._body, null, 2) || error;
-        this.errorMessage += message + "";
+        if (error.type === 3) { return; }
+        const message = error.message || JSON.stringify(error._body, null, 2) || error;
+        this.errorMessage += message + '';
         console.log(message);
     }
 
     newCondition(rule: IRule): void {
-        if (!rule.conditions)
+        if (!rule.conditions) {
             rule.conditions = [];
+        }
         rule.conditions.push(<ICondition>{});
     }
 
     newAction(rule: IRule): void {
-        if (!rule.actions)
+        if (!rule.actions) {
             rule.actions = [];
+        }
         rule.actions.push(<IAction>{});
     }
 
     newBodyEntry(action: IAction): void {
-        if (!action.body)
+        if (!action.body) {
             action.body = [];
+        }
         action.body.push(<IBody>{});
     }
 
@@ -82,24 +84,24 @@ export class RulesListComponent implements OnInit {
     }
 
     removeBody(action: IAction, body: IBody): void {
-        let index: number = action.body.findIndex(x => x.key == body.key);
-        if (index > -1)
+        const index: number = action.body.findIndex(x => x.key == body.key);
+        if (index > -1) {
             action.body.splice(index, 1);
+        }
     }
 
     removeRule(rule: IRule): void {
         if (rule.id) {
             this._rulesService.delete(rule.id)
             .subscribe(
-                ok => 
-                {
-                    this.parseMessage(ok);                
-                    if (!this.errorMessage.includes("error"))
+                ok => {
+                    this.parseMessage(ok);
+                    if (!this.errorMessage.includes('error')) {
                         this.initRules();
+                    }
                 }, this.parseMessage);
-        }
-        else {
-            let index = this.rules.findIndex(x => x.name == rule.name);
+        } else {
+            const index = this.rules.findIndex(x => x.name == rule.name);
             if (index > -1) {
                 this.rules.splice(index, 1);
             }
@@ -107,13 +109,14 @@ export class RulesListComponent implements OnInit {
     }
 
     removeAction(rule: IRule, action: IAction): void {
-        let index: number = rule.actions.findIndex(x => x.address == action.address);
-        if (index > -1)
+        const index: number = rule.actions.findIndex(x => x.address == action.address);
+        if (index > -1) {
             rule.actions.splice(index, 1);
+        }
     }
 
     removeCondition(rule: IRule, condition: ICondition): void {
-        let index: number = rule.conditions.findIndex(x => x.address == condition.address && x.operator == condition.operator);
+        const index: number = rule.conditions.findIndex(x => x.address == condition.address && x.operator == condition.operator);
         if (index > -1) {
             rule.conditions.splice(index, 1);
         }
--- src/app/rules/rules.service.ts
+++ src/app/rules/rules.service.ts
@@ -1,116 +1,123 @@
-import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
-import { HttpHeaders } from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Http, Response} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
+import {HttpHeaders} from '@angular/common/http';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/timeout';
-
-import { IRule, IBody, IAction, IPayload, IPayloadAction } from './rule'
+import {IRule, IBody, IAction, IPayload, IPayloadAction} from './rule';
 
 @Injectable()
 export class RulesService {
-    private _bridgeUrl = 'http://192.168.2.1/api/BDJo7SGB-6KsWHHAaXZidJNuboQejknxnh6ruEWe/rules';
-    private _dummyUrl = 'api/rules/dummy.json';
-    private _responseUrl = 'api/rules/response.json';
+  private _bridgeUrl = 'http://192.168.178.22/api/BDJo7SGB-6KsWHHAaXZidJNuboQejknxnh6ruEWe/rules';
+  private _dummyUrl = 'api/rules/dummy.json';
+  private _responseUrl = 'api/rules/response.json';
 
-    private httpOptions = {
-        headers: new HttpHeaders({
-          'Content-Type':  'application/json'
-        })
-      };
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
 
-    constructor(private _http: Http) { }
+  constructor(private _http: Http) {
+  }
 
-    getRules(): Observable<IRule[]> {
-        return this._http.get(this._bridgeUrl)
-            .map(this.mapData)
-            .timeout(1000);
+  getRules(): Observable<IRule[]> {
+    return this._http.get(this._bridgeUrl)
+      .map(this.mapData)
+      .timeout(1000);
+  }
+
+  returnDummy(): Observable<IRule[]> {
+    return this._http.get(this._dummyUrl)
+      .map(this.mapData);
+  }
+
+  mapData = (response: Response): IRule[] => {
+    const json = response.json();
+    const result = <IRule[]>Object.keys(json).map(key => {
+      const value = <IRule>json[key];
+      value.id = +key;
+      for (const action of value.actions) {
+        action.body = this.convertToBodyArray(action.body);
+      }
+      return value;
+    });
+    return result;
+  }
+
+  save(rule: IRule): Observable<Response> {
+    if (rule.id) {
+      return this.putRule(rule);
     }
+    return this.postRule(rule);
+  }
 
-    returnDummy(): Observable<IRule[]> {
-        return this._http.get(this._dummyUrl)
-            .map(this.mapData);
-    }
+  private postRule(rule: IRule): Observable<Response> {
+    return this._http.post(this._bridgeUrl, this.mapToPayload(rule))
+      .timeout(2000);
+  }
 
-    mapData = (response: Response): IRule[] => {
-        let json = response.json();
-        let result = <IRule[]>Object.keys(json).map(key => {
-            let value = <IRule>json[key];
-            value.id = +key;
-            for (let action of value.actions)
-                action.body = this.convertToBodyArray(action.body);
-            return value;
-        });
-        return result;
-    }
+  private putRule(rule: IRule): Observable<Response> {
+    const postUrl = this._bridgeUrl + '/' + rule.id;
+    return this._http.put(postUrl, this.mapToPayload(rule))
+      .timeout(2000);
+  }
 
-    save(rule: IRule): Observable<Response> {
-        if (rule.id)
-            return this.putRule(rule);
-        return this.postRule(rule);
-    }
+  private mapToPayload(rule: IRule): IPayload {
+    const data = <IPayload>{};
+    data.name = rule.name;
+    data.actions = this.convertToPayloadAction(rule.actions);
+    data.conditions = rule.conditions;
+    data.status = rule.status;
+    console.log(JSON.stringify(data));
+    return data;
+  }
 
-    private postRule(rule: IRule): Observable<Response> {
-        return this._http.post(this._bridgeUrl, this.mapToPayload(rule))
-            .timeout(2000);
+  convertToPayloadAction(data: IAction[]): IPayloadAction[] {
+    const payloadActions = [];
+    for (const action of data) {
+      const payload = <IPayloadAction>{};
+      payload.address = action.address;
+      payload.method = action.method;
+      payload.body = {};
+      for (const body of action.body) {
+        payload.body[body.key] = this.convertToCorrectType(body.value);
+      }
+      payloadActions.push(payload);
     }
+    return payloadActions;
+  }
 
-    private putRule(rule: IRule): Observable<Response> {
-        let postUrl = this._bridgeUrl + '/' + rule.id;
-        return this._http.put(postUrl, this.mapToPayload(rule))
-            .timeout(2000);
+  convertToBodyArray(data: any): IBody[] {
+    const bodyEntries = [];
+    for (const key in data) {
+      if (data.hasOwnProperty(key)) {
+        bodyEntries.push({key: key, value: data[key]});
+      }
     }
+    return bodyEntries;
+  }
 
-    private mapToPayload(rule: IRule): IPayload {
-        var data = <IPayload>{};
-        data.name = rule.name;
-        data.actions = this.convertToPayloadAction(rule.actions);
-        data.conditions = rule.conditions;
-        data.status = rule.status;
-        console.log(JSON.stringify(data));
-        return data;
+  private convertToCorrectType = (val: string | number | boolean): string | number | boolean => {
+    if (typeof(val) === typeof(true)) {
+      return val;
     }
+    if (val === 'true') {
+      return true;
+    }
+    if (val === 'false') {
+      return false;
+    }
+    const num = +val;
+    if (!isNaN(num)) {
+      return num;
+    }
+    return val;
+  };
 
-    convertToPayloadAction(data: IAction[]): IPayloadAction[]
-    {
-        let payloadActions = [];
-        for (let action of data)
-        {
-            let payload = <IPayloadAction>{};
-            payload.address = action.address;
-            payload.method = action.method;
-            payload.body = {};
-            for (let body of action.body)
-                payload.body[body.key] = this.convertToCorrectType(body.value);
-            payloadActions.push(payload);
-        }
-        return payloadActions;
-    }
-
-    convertToBodyArray(data: Object): IBody[] {
-        let bodyEntries = [];
-        for (let key in data) {
-            bodyEntries.push({key: key, value: data[key]});
-        }
-        return bodyEntries;
-    }
-
-    private convertToCorrectType = (val: string|number|boolean): string|number|boolean => {
-        if (typeof(val) === typeof(true))
-            return val;
-        if (val == "true")
-            return true;
-        if (val == "false")
-            return false;
-        let num = +val;
-        if (!isNaN(num))
-            return num;
-        return val;
-    }
-
-    delete(id: number): Observable<Response> {
-        return this._http.delete(this._bridgeUrl + '/' + id)
-            .timeout(2000);
-    }
+  delete(id: number): Observable<Response> {
+    return this._http.delete(this._bridgeUrl + '/' + id)
+      .timeout(2000);
+  }
 }
