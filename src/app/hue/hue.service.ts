d01cb0b54fdd3beb51758f67cb1df4ae7bf55c7f
Matt Bultje <matthias.bultje@topicus.nl>
2019-09-13 14:06:34 +0200

    Webpack config voor dgram

--- src/app/hue/hue.service.ts
+++ src/app/hue/hue.service.ts
@@ -1,4 +1,6 @@
 import {Injectable} from '@angular/core';
+import * as huejay from 'huejay';
+import {from} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
---
+++ webpack.config.js
@@ -0,0 +1,5 @@
+resolve: {
+  alias: {
+    dgram: "dgram-browserify"
+  }
+}
